b8d24528f416d7204572c6b9157ada531d06555f
fahmibahh <124516377+fahmibahh@users.noreply.github.com>
2023-03-14 13:24:24 +0700

    Update NFIX.py

--- NFIX.py
+++ NFIX.py
@@ -9810,6 +9810,7 @@ class NostalgiaForInfinityX(IStrategy):
                     item_buy_logic.append((dataframe['ema_26'].shift() - dataframe['ema_12'].shift()) > (dataframe['open'] / 100))
                     item_buy_logic.append(dataframe['close'] < dataframe['ema_20'] * 0.934)
                     item_buy_logic.append(dataframe['r_480_1h'] < -16.0)
+                    item_buy_logic.append(dataframe['cti'] < -0.8)
                     item_buy_logic.append(dataframe['volume'] < (dataframe['volume_mean_4'] * 5.0))
 
                 # Condition #12 - Semi swing. Local deeper dip. Uptrend.
